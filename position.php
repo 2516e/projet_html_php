7622763ddafabd231190b4223202c0f13358e5ec
2516e <mahleb.enzo@gmail.com>
2024-05-30 16:57:01 +0200

    ok

---
+++ position.php
@@ -0,0 +1,25 @@
+<?php require __DIR__ . "/public/header.html.php"?>
+
+<main>
+    <div id="poscontainer">
+            <h1 id="posh1">Position</h1>
+        </div>
+            <br>
+            <br>
+            <br>
+            <br>
+        <div id="posp">
+            <p>Centrer un élément de type block</p>
+        </div>
+            <br>
+        <div class="centerblock"></div>
+            <br>
+            <br>
+            <br>
+            <br>
+        <p id="absp">Position absolute</p>
+        <div class="abs"></div>
+    </div>
+</main>
+
+<?php include "public/footer.html.php"?>
--- public/nav.html.php
+++ public/nav.html.php
@@ -1,15 +1,15 @@
 <nav>
     <ul class="link_list">
-        <li><a href="">Structure</a></li>
+        <li><a href="index.php">Structure</a></li>
         <li><a href="">Texte</a></li>
         <li><a href="">Selecteur</a></li>
         <li><a href="">Image</a></li>
-        <li><a href="">Position</a></li>
+        <li><a href="position.php">Position</a></li>
         <li><a href="">Zoning</a></li>
         <li><a href="">Tableau</a></li>
         <li><a href="">Liste</a></li>
         <li><a href="">Lien & menu</a></li>
-        <li><a href="http://localhost/projet_html_php/formulaire.php">Formulaire</a></li>
+        <li><a href="formulaire.php">Formulaire</a></li>
         <li><a href="">Multimedia</a></li>
         <li><a href="">Animation</a></li>
     </ul>
