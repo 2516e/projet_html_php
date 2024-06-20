57b268746d2d300b3f89bd19a41c911f9e1bdde4
2516e <mahleb.enzo@gmail.com>
2024-06-20 17:00:07 +0200

    20.06.24

--- image.php
+++ image.php
@@ -1,7 +1,13 @@
 <?php require __DIR__ . "/public/header.html.php"?>
 
 <main>
-    
+    <div class="firstrow">
+        <h1>Images</h1>
+        <div class="firstrow_content">
+            <h2>Image de fond en CSS</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nihil adipisci laudantium illum similique, autem sit eaque voluptatibus accusamus quod aliquam perferendis repellat, porro totam tempore minima error ad. Adipisci temporibus minima molestiae asperiores id reprehenderit, modi delectus labore culpa quos quasi ad. Quis quaerat debitis omnis adipisci doloribus. Est.</p>
+        </div>
+    </div>
 </main>
 
 <?php include "public/footer.html.php"?>
